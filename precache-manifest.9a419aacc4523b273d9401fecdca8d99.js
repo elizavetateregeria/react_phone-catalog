bded1edbfad70713a76760a9cf148a244ade9606
felsi13 <13felsi@gmail.com>
2019-08-19 11:52:18 +0300

    Updates

---
+++ precache-manifest.9a419aacc4523b273d9401fecdca8d99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a761f7e568d6484b6146036535549280",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "b5d4be029b1271825c1b",
+    "url": "/react_phone-catalog/static/css/main.700b67a9.chunk.css"
+  },
+  {
+    "revision": "179ca294a5d394f12a68",
+    "url": "/react_phone-catalog/static/js/2.0fe2bdb8.chunk.js"
+  },
+  {
+    "revision": "b5d4be029b1271825c1b",
+    "url": "/react_phone-catalog/static/js/main.8c1869e6.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
